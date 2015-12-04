5802317b8131fbe2813f70725f9ba2f1ccb788cf
Cam Jackson <camjackson89@gmail.com>
2015-12-05 10:25:19 +1100

    Delete unnecessary run script

--- run.sh
+++
@@ -1,4 +0,0 @@
-rustc --color always --crate-type=dylib src/lib.rs
-#rustc -Z unstable-options --pretty expanded --extern jsx=libjsx.so src/main.rs
-rustc --color always --extern jsx=libjsx.so src/main.rs
-./main
